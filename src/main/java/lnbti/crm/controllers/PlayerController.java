3946ffb6b5f8797b7dca1082b6fe82d4aed1704a
Eternatus9999 <eternatus9999@gmail.com>
2025-06-21 07:49:56 +0530

    Add addPlayer method to the player controller

--- src/main/java/lnbti/crm/controllers/PlayerController.java
+++ src/main/java/lnbti/crm/controllers/PlayerController.java
@@ -1,5 +1,6 @@
 package lnbti.crm.controllers;
 
+import lnbti.crm.dto.Player;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,5 +10,9 @@ public class PlayerController {
     @GetMapping("/player-id/")
     public String getPlayerById(){
         return "hello World!";
+    }
+    @PostMapping("/player-add/")
+    public void addPlayer(@RequestBody Player player){
+        System.out.print(player);
     }
 }
