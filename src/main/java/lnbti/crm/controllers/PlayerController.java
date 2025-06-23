b71799690d2b774df3be1ab857411ea70c66dcb8
Eternatus9999 <eternatus9999@gmail.com>
2025-06-23 08:19:28 +0530

    Add updatePlayer method to the player controller

--- src/main/java/lnbti/crm/controllers/PlayerController.java
+++ src/main/java/lnbti/crm/controllers/PlayerController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 @RequestMapping("/player")
 public class PlayerController {
-    @GetMapping("/player-id/")
-    public String getPlayerById(){
-        return "hello World!";
+    @GetMapping("/player-id/{id}")
+    public Player getPlayerById(@PathVariable String id){
+        return new Player(2,"Chathusha",20,"eternatus9999@gmail.com","1234","Male");
     }
     @PostMapping("/player-add/")
     public void addPlayer(@RequestBody Player player){
@@ -18,5 +18,9 @@ public class PlayerController {
     @DeleteMapping("/player-delete/{id}")
     public void deletePlayer(@PathVariable long id){
         System.out.print(id);
+    }
+    @PutMapping("player-update/")
+    public String updatePlayer(@RequestBody Player player){
+        return "Player Updated Successfully!";
     }
 }
