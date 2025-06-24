b9ef3500aae8b03f0ea68f72718e12650f840d2c
Eternatus9999 <eternatus9999@gmail.com>
2025-06-24 09:02:23 +0530

    Testing hibernate

--- src/main/java/lnbti/crm/service/impl/PlayerServiceImpl.java
+++ src/main/java/lnbti/crm/service/impl/PlayerServiceImpl.java
@@ -1,7 +1,11 @@
 package lnbti.crm.service.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lnbti.crm.dto.Player;
+import lnbti.crm.entity.PlayerEntity;
+import lnbti.crm.repository.PlayerRepository;
 import lnbti.crm.service.PlayerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +13,12 @@ import java.util.List;
 
 @Primary
 @Service
+@RequiredArgsConstructor
 public class PlayerServiceImpl implements PlayerService {
+
+    final PlayerRepository playerrepository;
+    final ObjectMapper objectmapper;
+
     @Override
     public Player getPlayerById(long id) {
         return new Player(1,"Chathusha",20,"eternatus9999@gmail.com","12345","Male");
@@ -22,6 +31,7 @@ public class PlayerServiceImpl implements PlayerService {
 
     @Override
     public String addPlayer(Player player) {
+        playerrepository.save(objectmapper.convertValue(player,PlayerEntity.class));
         return "Added Successfully!";
     }
 
